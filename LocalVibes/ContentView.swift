7400411bd47f4767cfc1f0535e0e2ea5ceccbb6f
Florian <florian.peters0306@gmail.com>
2023-10-11 11:30:08 +0200

    Initial Commit

---
+++ LocalVibes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalVibes
+//
+//  Created by Florian Peters on 11.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalVibes/LocalVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalVibesApp.swift
+//  LocalVibes
+//
+//  Created by Florian Peters on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct LocalVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
