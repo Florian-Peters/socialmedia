c57c1b6a38fab60769d3f207a1832fbb89fcdb7d
Florian <florian.peters0306@gmail.com>
2023-10-19 16:00:06 +0200

    - Hinzugefügte UI-Elemente für die Benutzeroberfläche - Callbacks für das Posten von Beiträgen - Verwaltung von Benutzerdaten mit AppStorage - Aktivierung der Cancel- und Post-Buttons

---
+++ LocalVibes/Model/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  LocalVibes
+//
+//  Created by Florian Peters on 19.10.23.
+//
+
+import SwiftUI
+
+struct Post: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Post()
+}
---
+++ LocalVibes/View Group/MainView/CreatNewPost.swift
@@ -0,0 +1,18 @@
+//
+//  CreatNewPost.swift
+//  LocalVibes
+//
+//  Created by Florian Peters on 19.10.23.
+//
+
+import SwiftUI
+
+struct CreatNewPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatNewPost()
+}
